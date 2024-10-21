560433691c9e2a91963c586e9310350d0e210498
Antonio <antonio.carnevalli@usp.br>
2024-10-21 14:53:13 -0300

    Implementar visualização do histórico de filmes

--- movies/views.py
+++ movies/views.py
@@ -5,7 +5,13 @@ from django.http import HttpResponse
 from .temp_data import movie_data
 
 def detail_movie(request, movie_id):
-    context = {'movie': movie_data[movie_id - 1]}
+    movie = get_object_or_404(Movie, pk=movie_id)
+    if 'last_viewed' not in request.session:
+        request.session['last_viewed'] = []
+    request.session['last_viewed'] = [movie_id] + request.session['last_viewed']
+    if len(request.session['last_viewed']) > 5:
+        request.session['last_viewed'] = request.session['last_viewed'][:-1]
+    context = {'movie': movie}
     return render(request, 'movies/detail.html', context)
 
 from django.shortcuts import render
@@ -41,3 +47,16 @@ def create_movie(request):
             reverse('movies:detail', args=(len(movie_data), )))
     else:
         return render(request, 'movies/create.html', {})
+    
+class MovieListView(generic.ListView):
+    model = Movie
+    template_name = 'movies/index.html'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        if 'last_viewed' in self.request.session:
+            context['last_movies'] = []
+            for movie_id in self.request.session['last_viewed']:
+                context['last_movies'].append(
+                    get_object_or_404(Movie, pk=movie_id))
+        return context
